0ea300651d0029376cc71b724ebce25ff0bc929e
jonatan <jona.cc.93@gmail.com>
2022-01-20 22:42:58 -0300

    [add]app funcionando

---
+++ cliente/src/Chat.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import ScrollToBottom from "react-scroll-to-bottom";
+
+const Chat = ({ socket, username, room }) => {
+  const [currenteMessage, setCurrenteMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  const sendMessage = async () => {
+    if (currenteMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currenteMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrenteMessage("");
+    }
+  };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
+
+  return (
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent) => {
+            return (
+              <div
+                className="message"
+                id={username === messageContent.author ? "you" : "other"}
+              >
+                <div>
+                  <div className="message-content">
+                    <p>{messageContent.message}</p>
+                  </div>
+                  <div className="message-meta"></div>
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
+              </div>
+            );
+          })}
+        </ScrollToBottom>
+      </div>
+      <div className="chat-footer">
+        <input
+          type="text"
+          value={currenteMessage}
+          placeholder="Type a message..."
+          onChange={(e) => {
+            setCurrenteMessage(e.target.value);
+          }}
+          onKeyPress={(e) => {
+            e.key === "Enter" && sendMessage();
+          }}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
--- servidor/src/index.js
+++ servidor/src/index.js
@@ -1,12 +1,38 @@
 const express = require("express");
-
 const app = express();
-const port = 3000;
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const port = 3000;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  },
 });
 
-app.listen(port, () => {
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log(`User Connected : ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID : ${socket.id} joined the room : ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User Disconnected : ${socket.id}`);
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
